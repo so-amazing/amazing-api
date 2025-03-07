e1613d4a3781159b11231b9a969bb95ff3ea9a20
kemboi22 <kemboielvis22@gmail.com>
2025-03-07 03:41:12 +0300

    feat: added Shops Models and migration

---
+++ app/models/shop.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class Shop extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare location: string
+
+  @column()
+  declare types: object
+
+  @column()
+  declare isVerified: boolean
+
+  @column()
+  declare deliveryInfo: object
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
