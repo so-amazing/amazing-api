8ad1dd3bb70d9b7d562fca97b291513e4f22da8d
kemboi22 <kemboielvis22@gmail.com>
2025-03-09 14:07:02 +0300

    feat: added product Models migration and controller

---
+++ app/models/product.ts
@@ -0,0 +1,56 @@
+
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import Shop from './shop.js'
+import Category from './category.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+
+export default class Product extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare shopId: number
+
+  @column()
+  declare categoryId: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare images: object | null
+
+  @column()
+  declare price: number
+
+  @column()
+  declare isNegotiable: boolean
+
+  @column()
+  declare views: number | null
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Shop)
+  declare shop: BelongsTo<typeof Shop>
+
+  @belongsTo(() => Category)
+  declare category: BelongsTo<typeof Category>
+}
+
