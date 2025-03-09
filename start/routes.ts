62aa5be750b5e51645607f0b1c46e39482fc6aac
kemboi22 <kemboielvis22@gmail.com>
2025-03-09 15:48:58 +0300

    feat: added Routes

--- start/routes.ts
+++ start/routes.ts
@@ -10,10 +10,16 @@
 const UsersController = () => import("#controllers/users_controller");
 import router from "@adonisjs/core/services/router";
 import { middleware } from "./kernel.js";
+const CategoriesController = () => import("#controllers/categories_controller");
+const ProductsController = () => import("#controllers/products_controller");
+const ShopsController = () => import("#controllers/shops_controller");
 
 router
   .group(() => {
     router.resource("user", UsersController).apiOnly();
+    router.resource("categories", CategoriesController).apiOnly();
+    router.resource("products", ProductsController).apiOnly();
+    router.resource("shops", ShopsController).apiOnly();
   })
   .prefix("api")
   .use(
