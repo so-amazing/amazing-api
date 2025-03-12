d3f94b09cff27a1beab121adf2396ba06b3cd30a
kemboi22 <kemboielvis22@gmail.com>
2025-03-12 04:12:33 +0300

    feat: added Api Documentation

--- start/routes.ts
+++ start/routes.ts
@@ -15,6 +15,22 @@ const CategoriesController = () => import("#controllers/categories_controller");
 const ProductsController = () => import("#controllers/products_controller");
 const ShopsController = () => import("#controllers/shops_controller");
 
+// Register Api Routes
+import AutoSwagger from "adonis-autoswagger";
+import swagger from "#config/swagger";
+
+// returns swagger in YAML
+router.get("/swagger", async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger);
+});
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get("/docs", async () => {
+  //return AutoSwagger.default.ui("/swagger", swagger);
+  return AutoSwagger.default.scalar("/swagger"); //to use Scalar instead. If you want, you can pass proxy url as second argument here.
+  //return AutoSwagger.default.rapidoc("/swagger", "view"); // to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+});
+
 router
   .group(() => {
     router.resource("users", UsersController).apiOnly();
